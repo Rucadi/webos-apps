ac3751d6c5f2a3564f975799206adc92d60d085e
rucadi <ruben.cano96@gmail.com>
2024-05-25 01:35:09 +0200

    Use ubuntu for x86

--- common/default.nix
+++ common/default.nix
@@ -9,8 +9,8 @@ let
       imageDigest = "sha256:65b09f9b6a89bffc7dac58ac1445c5492fb6d421e7717896666d531cce405472";
     };
     x86_64-linux = {
-      sha256 = "sha256-jwU7BxoFbjYCUoG+qpqV4Nm0ba3Asi4HyRW6Y76KSHU=";
-      imageDigest = "sha256:ba08ecc7ea9a9df8b009419fb65b5bb96e530582b67dcb12de13581a0feaea71";
+      sha256 = "sha256-PQ5693Zv7XH3Znl8GVsv+qrPlRIciNatqtLYfqTrGUU=";
+      imageDigest = "sha256:f8574688f03d9a2bf84662cdae4d09dd4c82bd7789ec000cd11df7243f9d8494";
     };
   };
 
